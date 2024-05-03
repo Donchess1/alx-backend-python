19b1133021766b57ce07fb1e8f5ca275f73a1088
Donchess1 <stepabod@.com>
2024-05-03 20:49:14 +0000

    shots

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""function for sum_list"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """compute sum of list"""
+
+    return float(sum(input_list))
+
